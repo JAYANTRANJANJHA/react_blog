b1119313436ee62058909c11219c569b67507e3e
JAYANT RANJAN JHA <121686579+JAYANTRANJANJHA@users.noreply.github.com>
2025-08-04 18:24:26 +0530

    react-router

---
+++ public/routes1.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router"
+
+
+export default function App() {
+
+
+    return (
+        <>
+            <BrowserRouter>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
+
+
+                <Routes>
+                    <Route path="/" element={<h1>Home</h1>} />
+                    <Route path="/about" element={<h1>About</h1>} />
+                    <Route path="/contact" element={<h1>Contact</h1>} />
+
+
+                </Routes>
+            </BrowserRouter>
+
+
+
+
+
+        </>
+
+
+    )
+}
