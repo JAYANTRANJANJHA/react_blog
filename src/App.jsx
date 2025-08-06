b16f3eed5a1dea00179962695eb633c4199889b7
JAYANT RANJAN JHA <121686579+JAYANTRANJANJHA@users.noreply.github.com>
2025-08-06 19:50:53 +0530

    form validation-inputfield

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,51 @@
 
+import { useState } from 'react'
 import './App.css'
 
 export default function App() {
 
-return (
-    <>
-        
-    </>
+    const [name, setName] = useState('');
+    const [nameErr, setNameErr] = useState('');
 
-)
+     const [password, setpassword] = useState('');
+    const [passErr, setPassErr] = useState('');
+
+    const handleName=(evt)=>{
+        console.log(evt.target.value)
+        if(evt.target.value.length>20){
+          setNameErr("Max Char Limit Exceeded")  
+        }else{
+            setNameErr()
+        }
+    }
+
+    const handlePassword=(evt)=>{
+        let regex=/^[A-Z0-9a-z]+$/i;
+        if(regex.test(evt.target.value)){
+            setPassErr()
+        }else{
+            setPassErr("No Special character Allowed")
+            
+        }
+    }
+    return (
+        <>
+
+            <div>
+                <input className={nameErr?'error':''} type="text" onChange={handleName} placeholder='Name' />
+                <span>{nameErr && nameErr}</span>
+                <br /><br />
+                <input className={passErr?'error':''} type="text" placeholder='Password' onChange={handlePassword} />
+                <span>{passErr && passErr}</span>
+                <br /><br />
+                <button disabled={passErr||nameErr}>Login</button>
+                <br /><br />
+
+            </div>
+
+        </>
+
+    )
 }
 
 
